b0d0144bdd6a19e271e1b20d72ee0ebb69a22169
Tim Vuong <74928150+timmyvgit@users.noreply.github.com>
2022-04-14 10:23:38 -0500

    update

---
+++ functions.test.ts
@@ -0,0 +1,14 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+    // CODE HERE
+    test('shuffleArray should return an array', () => {
+        lett arr = [1,2,3,4,5]
+        let result = shuffleArray(arr)
+        expect(result).toHaveProperty('length')
+    })
+    test('shuffleArray should return an array', () => {
+        lett arr = [1,2,3,4,5]
+        let result = shuffleArray(arr)
+        expect(
+})
